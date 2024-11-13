dd801ea41cabc4c249aab88ac8c310e105466553
kpeter <akiralypeti@gmail.com>
2024-11-13 18:46:06 +0100

    deleted:    description.txt 	modified:   programozasi_alapismeretek/data/datahandler.py 	modified:   programozasi_alapismeretek/streamlit_app.py

--- programozasi_alapismeretek/streamlit_app.py
+++ programozasi_alapismeretek/streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 import analysis.linear_regression
 import data.datamodel
-import visualization.chart
+import visual.chart
 
 
 # ? remove unused main
@@ -14,12 +14,12 @@ def main() -> None:
 def run_streamlit_app() -> None:
 
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
-    ksh_data = data.data_model.StatisticalData("data/stadat-lak0001.xlsx")
+    ksh_data = data.datamodel.StatisticalData("data/stadat-nep0001.xlsx")
     lr_model = analysis.linear_regression.FittedModel(ksh_data.lr_x, ksh_data.lr_y)
-    lr_chart = visualization.chart_generator.generate_lr_chart(
+    lr_chart = visual.chart.generate_lr_chart(
         ksh_data.lr_x, ksh_data.lr_y, lr_model.prediction, axes[0, 0]
     )
-    line_chart = visualization.chart_generator.generate_line_chart(
+    line_chart = visual.chart.generate_line_chart(
         ksh_data.line_x, ksh_data.line_y, axes[0, 1]
     )
     plt.tight_layout()  # Fixes clipping on lower charts
